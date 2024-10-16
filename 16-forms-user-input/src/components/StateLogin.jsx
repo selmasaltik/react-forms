7778d6c9dbb183eae99697e48466d77eb9ef84d1
selmasaltik <selmasaltiik@gmail.com>
2024-10-16 16:14:57 +0300

    Validating Input on Every Keystroke via State

--- 16-forms-user-input/src/components/StateLogin.jsx
+++ 16-forms-user-input/src/components/StateLogin.jsx
@@ -9,6 +9,9 @@ export default function Login() {
     password: ''
   });
 
+  const emailIsInvalid = 
+    enteredValues.email !== '' && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -55,6 +58,9 @@ export default function Login() {
             value={enteredValues.email}
             autoComplete="off"
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
