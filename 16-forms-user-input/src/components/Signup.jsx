6f40198c696c4ae6771b5b15f3b63e9aa83af5ed
selmasaltik <selmasaltiik@gmail.com>
2024-10-16 18:33:03 +0300

    Mixing Custom & Built-in Validation Logic

--- 16-forms-user-input/src/components/Signup.jsx
+++ 16-forms-user-input/src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -6,6 +10,12 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll('acquisition');
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if(data.password !== data['confirm-password']) {
+      setPasswordsAreNotEqual(true);
+      return;
+    }
+
     console.log(data);
 
     // event.target.reset();
@@ -48,6 +58,9 @@ export default function Signup() {
             name="confirm-password"
             autoComplete="off"
           />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>Passwords must match.</p>}
+          </div>
         </div>
       </div>
 
